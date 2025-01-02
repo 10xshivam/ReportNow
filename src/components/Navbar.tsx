2e91c723a03208c2cde40f5ab8cdea7d0a14d87a
Shivam <kumarshivam3788@gmail.com>
2025-01-03 03:06:46 +0530

    feat: add Navbar component and update layout with new logo and improved structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const menuItems = [
+    {
+        menuOption: "Submit Report",
+        menuLink: "/submit-report"
+    },
+    {
+        menuOption: "Track Report",
+        menuLink: "/track-report"
+    },
+    {
+        menuOption: "How it works",
+        menuLink: "/how-it-works"
+    },
+    {
+        menuOption: "Resources",
+        menuLink: "/resources"
+    },
+]
+
+export default function Navbar() {
+  return (
+    <nav className='border-b w-full h-16 border-white/10 bg-black/60 backdrop-blur-xl items-center flex px-7 justify-between'>
+        <div className='flex justify-center items-center gap-2'>
+            <Image src="/logo.png" alt="logo" width={30} height={30}/>
+            <Link href={'/'} className='text-lg font-bold'>Safe Report</Link>
+        </div>
+        <div className='gap-10 flex justify-center items-center'>
+            {menuItems.map((menuItem)=>(
+                <Link 
+                key={menuItem.menuOption}
+                href={menuItem.menuLink}
+                className='text-white/65 hover:text-white text-md transition-colors'
+                >
+                    {menuItem.menuOption}
+                </Link>
+            ))}
+        </div>
+        <div className='px-3 py-1.5 rounded-full ring-1 ring-red-500/50 bg-red-500/10 text-red-500 flex justify-center items-center hover:bg-red-500/20 transition-all'>
+            <div className='w-2 inline-block bg-red-600 rounded-full h-2 mr-2 animate-pulse'/>
+            <Link href={'tel:112'} className='text-sm '>
+            National Emergency Helpline - 112
+            </Link>
+        </div>
+    </nav>
+  )
+}

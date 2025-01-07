77cca85a8d5abb0a815bc9d3e00e71b5ca6fd8c2
Shivam <kumarshivam3788@gmail.com>
2025-01-07 20:51:37 +0530

    feat: enhance ReportSubmitted component with copy to clipboard functionality and improved UI

--- src/components/report/ReportSubmitted.tsx
+++ src/components/report/ReportSubmitted.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+"use client";
+import { Check, Clipboard } from "lucide-react";
+import { useState } from "react";
 
 interface ReportSubmittedProps {
-    data:string;
+  data: string;
 }
 
-export default function ReportSubmitted({data}:ReportSubmittedProps) {
+export default function ReportSubmitted({ data }: ReportSubmittedProps) {
+  const reportId = data || "ERROR-ID-NOT-FOUND";
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(reportId);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 3000);
+    } catch (error) {
+      console.error("Failed to copy:", error);
+    }
+  };
+
   return (
-    <div>ReportSubmitted {data}</div>
-  )
+    <div className="flex flex-col items-center ">
+      <div className="p-3 bg-green-500/10 rounded-full w-fit">
+        <Check className="text-green-500 h-16 w-16" />
+      </div>
+      <h3 className="text-xl font-medium mt-4">
+        Report Successfully Submitted
+      </h3>
+      <p className="text-sm text-zinc-400 my-2 ">
+        Your report has been securely transmitted to law enforcement
+      </p>
+      <div className="flex flex-col items-center mt-3 bg-zinc-800/50 p-6 space-y-3 rounded-lg max-w-md mx-auto text-center">
+        <p className="text-md font-medium">Your Report ID</p>
+        <div className="bg-zinc-900 p-3 rounded w-full relative">
+          <code className="text-sky-500 text-lg">{reportId}</code>
+          <div
+            className="absolute inline bg-sky-500/5 p-1.5 rounded-lg top-[10px] right-3 hover:bg-sky-500/10"
+            onClick={copyToClipboard}
+          >
+            {isCopied ? (
+              <Check className="text-sky-500 h-5 w-5" />
+            ) : (
+              <Clipboard className="text-sky-500 w-5 h-5" />
+            )}
+          </div>
+        </div>
+        <p className="mt-2 text-sm text-zinc-400">
+          Save this ID to check your report status or communicate securely with
+          law enforcement
+        </p>
+      </div>
+    </div>
+  );
 }

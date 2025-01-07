d82e1dec40cd59e2d7840caf4d3fff2d2bfb6bff
Shivam <kumarshivam3788@gmail.com>
2025-01-08 02:38:07 +0530

    refactor: update GET function signature and improve response structure in report details API

--- src/app/api/reports/[reportId]/details/route.ts
+++ src/app/api/reports/[reportId]/details/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET({params}:{params:{reportId:string}}){
+export async function GET(req:Request,{params}:{params:{reportId:string}}){
     const reportId = params.reportId
     try {
         const report = await prisma.report.findUnique({
@@ -14,9 +14,7 @@ export async function GET({params}:{params:{reportId:string}}){
             return NextResponse.json({error: "Report not found"}, {status: 404})
         }
 
-        console.log(report)
-
-        return NextResponse.json({data:report},{status:200})
+        return NextResponse.json({report},{status:200})
     } catch (error) {
         console.error("Error fetching report details:", error)
         return NextResponse.json({error: "Failed to fetch report details"},{status: 500})

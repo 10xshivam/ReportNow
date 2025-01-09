c40b5149d6d9db91446c8259b400b6c97415ee28
Shivam <kumarshivam3788@gmail.com>
2025-01-09 18:06:12 +0530

    feat: add GitHub provider to NextAuth and implement admin role validation in session

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -1,8 +1,20 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions} from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./prisma";
 import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      role?: string | null;
+    };
+  }
+}
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -21,6 +33,28 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID!,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-      })
+    }),
+    GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
   ],
+  callbacks:{
+    async signIn({user}){
+        const adminEmail = process.env.ADMIN_EMAIL
+        if(user.email !== adminEmail){
+            return false
+        }
+        return true
+    },
+    async session({ session }) {
+        session.user.role = "admin"; // Assign the admin role
+        return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/signin",
+    error: "/auth/error",
+  },
 };

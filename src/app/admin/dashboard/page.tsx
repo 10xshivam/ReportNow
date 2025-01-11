60d1f2a10724ba7ad0644a049293b5bbda8dc6d1
Shivam <kumarshivam3788@gmail.com>
2025-01-11 17:54:37 +0530

    feat: implement report fetching and status update in admin dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -2,16 +2,63 @@
 import { Button } from '@/components/ui/button'
 import axios from 'axios'
 import { signOut, useSession } from 'next-auth/react'
-import React from 'react'
+import { useState } from 'react'
+import { Report, ReportStatus } from '@prisma/client'
+import { useToast } from '@/hooks/use-toast'
 
 export default function Dashboard() {
   const {data:session} = useSession()
+  const {toast} = useToast()
+  const [loading,setLoading] = useState(false)
+  const [reports,setReports] = useState<Report[]>([])
+  const [error,setError] = useState("")
+  
+  const fetchReports = async () => {
+    setError("")
+    setLoading(true)
+    try {
+      const response =  await axios.get('/api/reports')
+      setReports(response.data)
+      console.log(response.data)
+
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response?.data?.error) {
+        setError(err.response.data.error);
+      } else {
+        setError("Something went wrong.");
+      }
+      toast({
+        title: "Error in fetching reports",
+        description: error,
+      });
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const updateStatus = async (reportId:string,newStatus:ReportStatus) => {
+    try {
+      const response = await axios.patch(`/api/${reportId}`,{status:newStatus})
+      if(response.data){
+        fetchReports()
+      }
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response?.data?.error) {
+        setError(err.response.data.error);
+      } else {
+        setError("Something went wrong.");
+      }
+      toast({
+        title: "Error in updating report",
+        description: error,
+      });
+    }
+  }
+
   return (
   
-    <div className='flex justify-center items-center w-full min-h-screen flex-col'>
-      <Button onClick={() => signOut()}>Logout</Button>
-      <p>Name : {session?.user.name}</p>
-      <Button onClick={()=> axios.get('/api/reports')}>sent</Button>
+    <div className='flex  items-center w-full min-h-screen flex-col p-36'>
+      <h2>Dashboard</h2>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const menuItems = [
   {
@@ -22,31 +25,49 @@ const menuItems = [
 ];
 
 export default function Navbar() {
+  const { data: session } = useSession();
   return (
     <nav className="fixed top-0 border-b w-full h-16 border-white/10 bg-black/60 backdrop-blur-xl items-center flex px-7 justify-between z-50">
-      <div className="flex justify-center items-center gap-2">
-        <Image src="/logo.png" alt="logo" width={30} height={30} />
-        <Link href={"/"} className="text-lg font-bold">
-          Safe Report
-        </Link>
-      </div>
-      <div className="gap-10 flex justify-center items-center">
-        {menuItems.map((menuItem) => (
-          <Link
-            key={menuItem.menuOption}
-            href={menuItem.menuLink}
-            className="text-white/65 hover:text-white text-md transition-colors"
-          >
-            {menuItem.menuOption}
-          </Link>
-        ))}
-      </div>
-      <div className="px-3 py-1.5 rounded-full ring-1 ring-red-500/50 bg-red-500/10 text-red-500 flex justify-center items-center hover:bg-red-500/20 transition-all">
-        <div className="w-2 inline-block bg-red-600 rounded-full h-2 mr-2 animate-pulse" />
-        <Link href={"tel:112"} className="text-sm ">
-          National Emergency Helpline - 112
-        </Link>
-      </div>
+      {!session ? (
+        <>
+          <div className="flex justify-center items-center gap-2">
+            <Image src="/logo.png" alt="logo" width={30} height={30} />
+            <Link href={"/"} className="text-lg font-bold">
+              Safe Report
+            </Link>
+          </div>
+          <div className="gap-10 flex justify-center items-center">
+            {menuItems.map((menuItem) => (
+              <Link
+                key={menuItem.menuOption}
+                href={menuItem.menuLink}
+                className="text-white/65 hover:text-white text-md transition-colors"
+              >
+                {menuItem.menuOption}
+              </Link>
+            ))}
+          </div>
+          <div className="px-3 py-1.5 rounded-full ring-1 ring-red-500/50 bg-red-500/10 text-red-500 flex justify-center items-center hover:bg-red-500/20 transition-all">
+            <div className="w-2 inline-block bg-red-600 rounded-full h-2 mr-2 animate-pulse" />
+            <Link href={"tel:112"} className="text-sm ">
+              National Emergency Helpline - 112
+            </Link>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex justify-center items-center gap-2">
+            <Image src="/logo.png" alt="logo" width={30} height={30} />
+            <Link href={"/"} className="text-lg font-bold">
+              Safe Report
+            </Link>
+          </div>
+          <div className="flex justify-center items-center gap-4">
+            <p className="text-md text-white/50 hover:text-white font-medium cursor-pointer">{session.user.name}</p>
+            <Button className="bg-blue-600 hover:bg-blue-700" onClick={() => signOut()}>Logout</Button>
+          </div>
+        </>
+      )}
     </nav>
   );
 }

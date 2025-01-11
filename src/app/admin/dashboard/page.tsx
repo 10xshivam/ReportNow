d1dcddeac1854205aa6c6c4b1921ae05ba343ce0
Shivam <kumarshivam3788@gmail.com>
2025-01-11 13:27:55 +0530

    feat: add reports API endpoint and integrate report fetching in admin dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Button } from '@/components/ui/button'
+import axios from 'axios'
 import { signOut, useSession } from 'next-auth/react'
 import React from 'react'
 
@@ -7,9 +8,10 @@ export default function Dashboard() {
   const {data:session} = useSession()
   return (
   
-    <div className='flex justify-center items-center w-full min-h-screen'>
+    <div className='flex justify-center items-center w-full min-h-screen flex-col'>
       <Button onClick={() => signOut()}>Logout</Button>
       <p>Name : {session?.user.name}</p>
+      <Button onClick={()=> axios.get('/api/reports')}>sent</Button>
     </div>
   )
 }
